bae591475cb0b613de451356184f9df0a223921b
charafau <gorudonu@gmail.com>
2022-11-01 16:37:18 +0900

    test copy file

--- lib/widgets/bottom_buttons.dart
+++ lib/widgets/bottom_buttons.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+// const maxChunkSize = 1024 * 1024 * 99;
+const maxChunkSize = 1024;
 
 class BottomButtons extends StatelessWidget {
   const BottomButtons({
@@ -16,13 +22,42 @@ class BottomButtons extends StatelessWidget {
         children: [
           TextButton(onPressed: () {}, child: const Text('F3 View')),
           TextButton(onPressed: () {}, child: const Text('F4 Edit')),
-          TextButton(onPressed: () {}, child: const Text('F5 Copy')),
+          TextButton(
+              onPressed: () {
+                _getChunkStream();
+              },
+              child: const Text('F5 Copy')),
           TextButton(onPressed: () {}, child: const Text('F6 Move')),
           TextButton(onPressed: () {}, child: const Text('F7 New Folder')),
           TextButton(onPressed: () {}, child: const Text('F8 Delete')),
           TextButton(onPressed: () {}, child: const Text('ALT+F4 Exit')),
         ],
       ),
+    );
+  }
+
+  Future<void> _getChunkStream() async {
+    // print("reading from $start to $end");
+    File file = File('/home/charafau/cop/source/bigfile.img');
+    final stat = await file.stat();
+    // File file = File('/home/charafau/cop/source/lorem.txt');
+    final Stream<List<int>> read = file.openRead();
+    // final blob = file.slice(start, end);
+    // reader.readAsArrayBuffer(blob);
+    // await reader.onLoad.first;
+    int chunkSize = 64 * 1024;
+    int chunks = 0;
+    read.listen(
+      (List<int> event) {
+        // print('got chunk of data $event');
+        // print('got chunk of data');
+        chunks++;
+        print('Copied percentage: ${(chunkSize * chunks) / stat.size}');
+      },
+      onDone: () {
+        // print('got all chunks: $chunks');
+        print('copied whole file');
+      },
     );
   }
 }
